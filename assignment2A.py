d5f1d06ce694c605952d3b2cb4e7dccb159f3ae5
Matt <matthewf789@gmail.com>
2025-03-24 22:07:23 +1100

    Structure

---
+++ assignment2A.py
@@ -0,0 +1,30 @@
+import sys
+
+def depthFirstSearch():
+    return("Depth first search")
+
+def breadthFirstSearch():
+    return("Breadth first search")
+
+def greedyBestFirstSearch():
+    return("Greedy!")
+
+def aStarSearch():
+    return("A star!")
+
+def main():
+    file = open(sys.argv[1], "r")
+    nodes = file.read()
+    
+    match sys.argv[2].lower():
+        case "dfs":
+            print(depthFirstSearch())
+        case "bfs":
+            print(breadthFirstSearch())
+        case "gbfs":
+            print(greedyBestFirstSearch())
+        case "as":
+            print(aStarSearch())
+
+
+main()
