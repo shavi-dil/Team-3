5fcaf76f1c158deb38f4fc36287a5ca04c87a0ff
matt01 <matthewf789@gmail.com>
2025-04-01 15:45:36 +1100

    Added files for the classes and search functions

---
+++ Searches.py
@@ -0,0 +1,88 @@
+import functools
+import Node
+from collections import deque
+import PriorityQueue
+
+def memoize(fn, slot = None, maxsize = 32):
+    if slot:
+        def memoized_fn(obj, *args):
+            if hasattr(obj, slot):
+                return getattr(obj, slot)
+            else:
+                val = fn(obj, *args)
+                setattr(obj, slot, val)
+                return val
+    else:
+        @functools.lru_cache(maxsize = maxsize)
+        def memoized_fn(*args):
+            return fn(*args)
+        
+    return memoized_fn
+
+def breadth_first_graph_search(problem):
+    node = Node(problem.initial)
+    if problem.goal_test(node.state):
+        return node
+    frontier = deque([node])
+    explored = set()
+    
+    while frontier():
+        node = frontier.popleft()
+        explored.add(node.state)
+
+        for child in node.expand(problem):
+            if child.state not in explored and child not in frontier:
+                if problem.goal_test(child.state):
+                    return child
+                frontier.append(child)
+
+    return None
+
+def depth_first_graph_search(problem):
+    frontier = [Node(problem.initial)] #Stack
+
+    explored = set()
+    while frontier:
+        node = frontier.pop()
+
+        if problem.goal_test(node.state):
+            return node
+        explored.add(node.state)
+        frontier.extend(child for child in node.expand(problem)
+                        if child.state not in explored and child not in frontier)
+
+    return None
+
+def best_first_graph_search(problem, f, display = False):
+    #Specify h(x) = f(x) for greedy best first search
+    f = memoize(f, 'f')
+
+    node = Node(problem.initial)
+    frontier = PriorityQueue('min', f)
+    frontier.append(node)
+    explored = set()
+
+    while frontier:
+        node = frontier.pop()
+
+        if problem.goal_test(node.state):
+            if display:
+                print(len(explored), "paths have been expanded and", len(frontier), "paths remain in the frontier")
+            return node
+        
+        explored.add(node.state)
+
+        for child in node.expand(problem):
+            if child.state not in explored and child not in frontier:
+                frontier.append(child)
+            elif child in frontier:
+                if f(child) < frontier[child]:
+                    del frontier[child]
+                    frontier.append(child)
+
+    return None
+
+def astar_search(problem, h = None, display = False):
+    h = memoize(h or problem.h, 'h')
+
+    return best_first_graph_search(problem, lambda n: n.path_cost + h(n), display)
---
+++ problem.py
@@ -0,0 +1,22 @@
+class problem():
+    def __init__(self, initial, goal):
+        self.initial = initial
+        self.goal = goal
+
+    def actions(self, state):
+        raise NotImplementedError
+    
+    def result(self, state, action):
+        raise NotImplementedError
+    
+    def goal_test(self, state):
+        if isinstance(self.goal, list):
+            return any(x is state for x in self.goal)
+        else:
+            return state == self.goal
+        
+    def path_cost(self, c, state1, action, state2):
+        return c + 1
+    
+    def value(self, state):
+        raise NotImplementedError
