ad3768988fe4eb8d885574cca59fcb8b978c9a47
matt01 <matthewf789@gmail.com>
2025-04-01 15:38:27 +1100

    Converted searches to graph search

--- assignment2A.py
+++ assignment2A.py
@@ -191,27 +191,37 @@ def memoize(fn, slot = None, maxsize = 32):
         
     return memoized_fn
 
-def breadth_first_tree_search(problem):
-    frontier = deque([Node(problem.initial)])#Frontier is FIFO queue
-
-    while frontier:
+def breadth_first_graph_search(problem):
+    node = Node(problem.initial)
+    if problem.goal_test(node.state):
+        return node
+    frontier = deque([node])
+    explored = set()
+    
+    while frontier():
         node = frontier.popleft()
-        
-        if problem.goal_test(node.state):
-            return node
-        frontier.extend(node.expand(problem))
+        explored.add(node.state)
+
+        for child in node.expand(problem):
+            if child.state not in explored and child not in frontier:
+                if problem.goal_test(child.state):
+                    return child
+                frontier.append(child)
 
     return None
 
-def depth_first_tree_search(problem):
+def depth_first_graph_search(problem):
     frontier = [Node(problem.initial)] #Stack
 
+    explored = set()
     while frontier:
         node = frontier.pop()
 
         if problem.goal_test(node.state):
             return node
-        frontier.extend(node.expand(problem))
+        explored.add(node.state)
+        frontier.extend(child for child in node.expand(problem)
+                        if child.state not in explored and child not in frontier)
 
     return None
 
